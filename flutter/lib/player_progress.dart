9ce2f9481b0d022f01dfafb22d1b2703a8c7eaad
Ah Subhan <ah.subhan@gmail.com>
2026-07-26 18:59:44 +0700

    Nonaktifkan tombol progres pemain baru

--- flutter/lib/player_progress.dart
+++ flutter/lib/player_progress.dart
@@ -2,6 +2,15 @@ import 'game_engine.dart';
 
 String playerProgressKey(String key, String userId) => '${key}_$userId';
 
+bool hasMeaningfulProgress({
+  required bool hasStarted,
+  required int? playerLevel,
+  required int? playerScore,
+}) {
+  if (!hasStarted) return false;
+  return (playerLevel ?? 1) > 1 || (playerScore ?? 0) > 0;
+}
+
 int resolveInitialLevel({
   required bool startFromLevelOne,
   required bool hasStarted,
